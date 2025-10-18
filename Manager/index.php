abfa0c8c5bbe4b85c8e627e69c15a848e70612d6
Habraham96 <daramolaadewunmi@gmail.com>
2025-10-18 22:02:55 +0100

    oct 18

--- Manager/index.php
+++ Manager/index.php
@@ -271,7 +271,7 @@ try {
         <nav class="sidebar sidebar-offcanvas" id="sidebar">
           <ul class="nav">
             <li class="nav-item">
-              <a class="nav-link" href="index.html">
+              <a class="nav-link" href="index.php">
                 <i class="menu-icon bi bi-house-door-fill"></i>
                 <span class="menu-title">Home</span>
               </a>
